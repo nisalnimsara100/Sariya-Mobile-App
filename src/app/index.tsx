00a3d01141067aa4d08507764834158c3dee85c6
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-09-12 10:42:43 +0530

    Update button style and asset in index screen
    Changed the button class from 'rounded' to 'rounded-lg' in src/app/index.tsx for improved appearance. Updated icon.png asset.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
     <View>
       <Text className='text-4xl font-bold mt-20 text-center'>Screen Buttons</Text>
       <Link href="screens/welcome" asChild>
-        <Pressable className='bg-blue-500 p-4 rounded m-10'>
+        <Pressable className='bg-blue-500 p-4 rounded-lg m-10'>
           <Text className='text-white text-center'>Open Welcome Screen</Text>
         </Pressable>
       </Link>
