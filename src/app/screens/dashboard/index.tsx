40a839c294b408bb8a464bcc060a45fb6c8d37cc
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-09-12 21:03:17 +0530

    Adds navigation button to Dashboard screen
    Introduces a new button to navigate to the Dashboard screen.
    Updates the styling of the existing Welcome screen button for improved layout consistency.

---
+++ src/app/screens/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text className='text-4xl font-bold mt-20 text-center'>Dashboard</Text>
+    </View>
+  )
+}
+
+export default Dashboard
